c5c3fad037ee6be4a758bb57982982068fc1c21d
dghnn <dgh@DESKTOP-46EBGG8.localdomain>
2026-05-16 08:58:51 +0000

    update .csv report

--- audit/run_all.sh
+++ audit/run_all.sh
@@ -4,32 +4,39 @@ set -e
 
 mkdir -p report
 
+CSV_FILE="report/cis_report.csv"
+
+# init CSV
+echo "module,status,message" > $CSV_FILE
+
 echo "===== CIS DOCKER FULL AUDIT ====="
 
-echo "[MODULE 1] Host Docker Engine Audit"
-bash ./audit/01-host-docker-engine-audit.sh | tee report/module1-host.txt
+run_module () {
+    MODULE_NAME=$1
+    SCRIPT=$2
+    OUTPUT_FILE=$3
 
-echo "[MODULE 2] Daemon & OS Security Audit"
-bash ./audit/02-audit-daemon-os-security.sh | tee report/module2-daemon.txt
+    echo "[RUN] $MODULE_NAME"
 
-echo "[MODULE 3] Image & Supply Chain Audit"
-bash ./audit/03-image-supply-chain-audit.sh | tee report/module3-image.txt
+    bash "$SCRIPT" | tee "$OUTPUT_FILE" | while read line; do
 
-echo ""
-echo "===== SUMMARY =====" | tee report/summary.txt
+        if echo "$line" | grep -q "\[PASS\]"; then
+            echo "$MODULE_NAME,PASS,\"${line#*] }\"" >> $CSV_FILE
 
-echo "Module 1 (last 20 lines):" | tee -a report/summary.txt
-tail -n 20 report/module1-host.txt >> report/summary.txt
+        elif echo "$line" | grep -q "\[FAIL\]"; then
+            echo "$MODULE_NAME,FAIL,\"${line#*] }\"" >> $CSV_FILE
 
-echo "" >> report/summary.txt
+        elif echo "$line" | grep -q "\[WARN\]"; then
+            echo "$MODULE_NAME,WARN,\"${line#*] }\"" >> $CSV_FILE
+        fi
 
-echo "Module 2 (last 20 lines):" | tee -a report/summary.txt
-tail -n 20 report/module2-daemon.txt >> report/summary.txt
+    done
+}
 
-echo "" >> report/summary.txt
-
-echo "Module 3 (last 20 lines):" | tee -a report/summary.txt
-tail -n 20 report/module3-image.txt >> report/summary.txt
+run_module "module1" "./audit/01-host-docker-engine-audit.sh" "report/module1-host.txt"
+run_module "module2" "./audit/02-audit-daemon-os-security.sh" "report/module2-daemon.txt"
+run_module "module3" "./audit/03-image-supply-chain-audit.sh" "report/module3-image.txt"
 
 echo ""
 echo "===== DONE ====="
+echo "CSV REPORT: report/cis_report.csv"
